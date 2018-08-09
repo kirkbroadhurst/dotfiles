d8989957f45c4b20ae68d0033c56805903b12c70
Kirk Broadhurst <kirk.broadhurst@gmail.com>
2018-08-09 16:20:49 -0500

    add an install script for python, vim, git

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get -y install python python-pip python3 python3-pip
+
+sudo apt-get remove vim-tiny
+sudo apt-get update
+sudo apt-get install vim
+
+sudo apt-get install git
+sudo apt-get upgrade git
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
