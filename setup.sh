bb894dc9a750474edfdc920c420ea9ca701ec70d
Kirk Broadhurst <kirk.broadhurst@gmail.com>
2018-08-09 16:36:47 -0500

    conditionally add export to .bashrc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-echo "export VISUAL=vim" >> ~/.bashrc
-echo "export EDITOR=\"\$VISUAL\"" >> ~/.bashrc
+if grep -Fxq "export VISUAL=vim" ~/.bashrc
+then
+    echo "export VISUAL already exists"
+else
+    echo "export VISUAL=vim" >> ~/.bashrc
+fi
+
+if grep -Fxq "export EDITOR=\"\$VISUAL\"" ~/.bashrc
+then
+    echo "export EDITOR already exists"
+else
+    echo "export EDITOR=\"\$VISUAL\"" >> ~/.bashrc
+fi
 
 cp .vimrc ~
